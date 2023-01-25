cddcb9750467394c7e7137cd5f6ab342e62e72d3
umjiii <sarel087@protonmail.com>
2023-01-25 11:42:49 -0700

    isPrime method now works properly Implemented array10 Properly identified base and recursive cases in all methods Reformatted test outputs for readability Added header comment to program

--- src/RecursionReview.java
+++ src/RecursionReview.java
@@ -1,3 +1,14 @@
+/*
+Author: Stephen Arel
+Class: CSC240
+Assignment: Lab 0 Recursion Review
+Purpose: Using several methods, the goal of this program
+        is to help reinforce the concept of recursion and using it in
+        situations that would otherwise be trivial with other concepts such as loops.
+        This practice helps reinforce the understanding of different ways that
+        recursion can be used (evaluating characters of Strings, array elements, etc.).
+ */
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -5,37 +16,68 @@ public class RecursionReview
 {
     public static void main(String[] args) //main execution
     {
-        //Test cases for count3
+        //------Test cases for count3
+        System.out.println("\n---Testing count3---");
+
         System.out.println("count3(313) ==> " + count3(313));
         System.out.println("count3(3) ==> " + count3(3));
         System.out.println("count3(456) ==> " + count3(456));
 
-        //Test cases for multiply
+        //------Test cases for multiply
+        System.out.println("\n---Testing multiply---");
+
         System.out.println("multiply(0, 10) ==> " + multiply(0, 10));
         System.out.println("multiply(1, 5) ==> " + multiply(1, 5));
         System.out.println("multiply(18, 8) ==> " + multiply(18, 8));
 
-        //Test cases for isPrime
-        System.out.println("isPrime(919, 2) ==> ");
-        System.out.println("isPrime(50, 2) ==> ");
-        System.out.println("isPrime(2, 2) ==> ");
+        //------Test cases for isPrime
+        System.out.println("\n---Testing isPrime---");
 
-        //Test cases for dupVowel
+        System.out.println("isPrime(919, 2) ==> " + isPrime(919, 2));
+        System.out.println("isPrime(50, 2) ==> " + isPrime(50, 2));
+        System.out.println("isPrime(2, 2) ==> " + isPrime(2, 2));
+
+        //------Test cases for dupVowel
+        System.out.println("\n---Testing dupVowel---");
+
         System.out.println("dupVowel(\"hello\") ==> " + dupVowel("hello"));
         System.out.println("dupVowel(\"xxyy\") ==> " + dupVowel("xxyy"));
         System.out.println("dupVowel(\"abc\") ==> " + dupVowel("abc"));
+
+        //------Test cases for array10:
+        System.out.println("\n---Testing array10---");
+
+        //Arrays for testing
+        int[] arrayFirst = new int[3];
+            arrayFirst[0] = 10;
+            arrayFirst[1] = 20;
+            arrayFirst[2] = 31;
+
+        int[] arraySec = new int[2];
+            arraySec[0] = 11;
+            arraySec[1] = 10;
+
+        int[] arrayThird = new int[4];
+            arrayThird[0] = 1;
+            arrayThird[1] = 2;
+            arrayThird[2] = 3;
+            arrayThird[3] = 4;
+
+        System.out.println("array10([10, 20, 31]) ==> " + array10(arrayFirst));
+        System.out.println("array10([11, 10]) ==> " + array10(arraySec));
+        System.out.println("array10([1, 2, 3, 4]) ==> " + array10(arrayThird));
     }
 
     //-------methods------
 
-    //count3: Given a non-negative int n, recursively count the occurrences of 3 as a digit.
+    //------ count3: Given a non-negative int n, recursively count the occurrences of 3 as a digit.
     public static int count3(int num)
     {
         //ensure num is not negative. if so, throw new exception
         if (num < 0) throw new IllegalArgumentException("The integer used for this method must be greater than zero.");
 
         //base case
-        else if (num < 10 && num == 3) return 1;
+        else if (num == 3) return 1;
 
         //recursive case.
         else if (num > 9) return count3(num / 10) + count3(num % 10);
@@ -44,7 +86,7 @@ public class RecursionReview
         else return 0;
     }
 
-    //multiply: Accepts two numbers and performs multiplication by recursively adding the same number multiple times.
+    //------ multiply: Accepts two numbers and performs multiplication by recursively adding the same number multiple times.
     public static int multiply(int num, int times)
     {
         //recursive case
@@ -54,23 +96,26 @@ public class RecursionReview
         else return 0;
     }
 
-    /* isPrime: Determines if a number is prime. Recursively
+    /*------ isPrime: Determines if a number is prime. Recursively
     determines if the any number less the number are divisible by the number. */
     public static boolean isPrime(int numToCheck, int count)
     {
-        if (numToCheck % count == 0 && numToCheck != count)
-            return true;
+        //base case
+        if (numToCheck % count == 0 && numToCheck != count && numToCheck != 1)
+            return false;
 
-        else if (count != numToCheck)
+        //recursive case
+        else if (!(numToCheck >= count))
             return isPrime(numToCheck, count + 1);
 
+        //base case
         else
-            return false;
+            return true;
 
         
     }
 
-
+    //------ dupVowel: Given a String, recursively builds a new string where the vowels are duplicated.
     public static String dupVowel(String str)
     {
 
@@ -96,7 +141,7 @@ public class RecursionReview
             vowelsArray[3] = 'o';
             vowelsArray[4] = 'u';
 
-            //recursive case; if the letter in question (the first letter of the string) is a vowel
+            //recursive cases; if the letter in question (the first letter of the string) is a vowel
             if (new String(vowelsArray).indexOf(str.charAt(0)) != -1)
             {
                 return str.charAt(0) + "" + str.charAt(0) + "" + dupVowel(new String(str.substring(1)));
@@ -107,6 +152,36 @@ public class RecursionReview
             }
         }
         else return "";
+
     }
 
+    /*
+    ------ array10: Given an array of ints, recursively computes the number of times that
+    a multiple of 10 appears in the array. index is used to examine a specific element from the array.
+    Uses a helper function (array10Helper) to incorporate an index to count through elements in the array.
+     */
+    public static int array10(int[] nums)
+    {
+        return array10Helper(nums, nums.length - 1);
+    }
+
+    //array10Helper: Helper method to incorporate index to count through array elements.
+    public static int array10Helper(int[] nums, int index)
+    {
+        //recursive cases: if there are still values to evalulate
+        if (index > 0)
+        {
+            if (nums[index] % 10 == 0)
+                return 1 + array10Helper(nums, index - 1);
+            else
+                return array10Helper(nums, index - 1);
+        }
+
+        //base case: if index is at the last element to be evaluated and is a multiple of 10.
+        else if (nums[index] % 10 == 0)
+            return 1;
+        //base case: if the index is at the last element to be evaluated and is not a multiple of 10.
+        else
+            return 0;
+    }
 }
